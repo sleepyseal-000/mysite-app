ac249aa564ef1a79c364a8d49a2d370c03e1815c
sleepyseal-000 <56409287+sleepyseal-000@users.noreply.github.com>
2023-02-04 01:54:51 -0500

    "commit changes"

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class To_do(models.Model):
+    #Store action do to task 
+    content = models.TextField()
+
+class To_cook(models.Model):
+    #Store action do to task 
+    content = models.TextField()
+
+class To_wish(models.Model):
+    #Store action do to task 
+    content = models.TextField()
