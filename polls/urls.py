ebbce4fbbdbe87a849510e475dac8a1ceebcafd3
sleepyseal-000 <56409287+sleepyseal-000@users.noreply.github.com>
2023-02-09 18:13:49 -0500

    "added models"

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
+
+    path('insert_to_do/', views.insert_to_do_item, name='insert_to_do_item'),
+    path('insert_to_cook/', views.insert_to_cook_item, name='insert_to_cook_item'),
+    path('insert_to_wish/', views.insert_to_wish_item, name='insert_to_wish_item'),
+
+
+    path('delete_to_do/<int:to_do_id>/', views.delete_to_do_item, name='delete_to_do_item'),
+    path('delete_to_cook/<int:to_cook_id>/', views.delete_to_cook_item, name='delete_to_cook_item'),
+    path('delete_to_wish/<int:to_wish_id>/', views.delete_to_wish_item, name='delete_to_wish_item'),
+
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from .models import To_do, To_cook, To_wish
 
 # Create your views here. (View our webpages)
 
+
 def homepage(request):
-    return render(request,'index.html')
+    context= {
+        'to_do_items': To_do.objects.all(),
+        'to_cook_items': To_cook.objects.all(),
+        'to_wish_items': To_wish.objects.all()
+        }
+    return render(request,'polls/index.html', context)
+
+
+#Insert 
+def insert_to_do_item(request:HttpRequest):
+    todo = To_do(content = request.POST['content'])
+    todo.save()
+    return redirect('/')
+
+def insert_to_cook_item(request:HttpRequest):
+    todo = To_cook(content = request.POST['content'])
+    todo.save()
+    return redirect('/')
+
+def insert_to_wish_item(request:HttpRequest):
+    todo = To_wish(content = request.POST['content'])
+    todo.save()
+    return redirect('/')
+
+#Delete
+def delete_to_do_item(request, to_do_id):
+    to_do_delete = To_do.objects.get(id=to_do_id)
+    to_do_delete.delete()
+    return redirect('/')
+
+def delete_to_cook_item(request, to_cook_id):
+    to_cook_delete = To_cook.objects.get(id=to_cook_id)
+    to_cook_delete.delete()
+    return redirect('/')
+
+def delete_to_wish_item(request, to_wish_id):
+    to_wish_delete = To_wish.objects.get(id=to_wish_id)
+    to_wish_delete.delete()
+    return redirect('/')
